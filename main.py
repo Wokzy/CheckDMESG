e24f1fd568d4dc23fd6599f91da1358869d2045c
Yegor <yegor6741@gmail.com>
2022-02-10 09:47:49 +0700

    First

---
+++ main.py
@@ -0,0 +1,30 @@
+import os, shutil, glob
+
+def copy_dmesg():
+	os.chdir('/var/crash')
+	dir_lst = glob.glob('*/dmesg.*')
+
+	for file in dir_lst:
+		shutil.copyfile(file, f"/opt/var/log/crash-log/{file.split('/')[-1]}")
+
+def clean_crashlog():
+	os.chdir('/opt/var/log/crash-log')
+
+	print(sorted(glob.iglob('*'), key=os.path.getctime, reverse=True))
+	not_in_top_10 = sorted(glob.iglob('*'), key=os.path.getctime, reverse=True)[10::]
+
+	for file in not_in_top_10:
+		os.system(f'rm -rf {file}')
+
+def clean_crash():
+	os.chdir('/var/crash')
+
+	list_without_first_3 = sorted(glob.iglob('*/'), key=os.path.getctime, reverse=True)[3::]
+
+	for directory in list_without_first_3:
+		if "." not in directory:
+			os.system(f"rm -rf {directory}")
+
+copy_dmesg()
+clean_crash()
+clean_crashlog()
